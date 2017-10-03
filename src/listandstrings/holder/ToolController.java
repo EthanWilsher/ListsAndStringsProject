22f1db7ca992948fb008edc78bd7208d671cf108
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-10-03 08:43:44 -0600

    Added a donut class.

--- src/listandstrings/holder/ToolController.java
+++ src/listandstrings/holder/ToolController.java
@@ -1,6 +1,10 @@
 package listandstrings.holder;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class ToolController
 {
+	
 
 }
---
+++ src/listandstrings/model/Donut.java
@@ -0,0 +1,10 @@
+package listandstrings.model;
+
+public class Donut
+{
+	private boolean hasSprinkles;
+	private boolean isGlazed;
+	private String shape;
+	private String flavor;
+	private int holeCount;
+}
